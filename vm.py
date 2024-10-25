7cd0626f6f5b2d5f0afe773a6a0afafc88a5451e
Kumo <U202211792@hust.eud.cn>
2024-10-25 17:02:38 +0800

    add vote

--- vm.py
+++ vm.py
@@ -3,12 +3,15 @@ import math
 import numpy as np
 import random
 import time
+import pdb 
 from tqdm import tqdm
 
 
 import torch
 import torch.nn as nn
 import torch.optim as op
+import torch.nn.functional as F
+from torch.nn.parameter import Parameter
 
 def polynomial_value(coefficients, x):
     """
@@ -24,7 +27,7 @@ def polynomial_value(coefficients, x):
         result += coefficients[i] * (x ** (i))
     return round(result)
 
-def search(b,L,waccm,data_list,test,label,init_weight):
+def search(b,L,waccm,data_list,test,label,init_weight,num = 16432):
     # bias = polynomial_value(init_weight,b)
     # print(bias)
     for _ in tqdm(range(b**L)): 
@@ -39,7 +42,7 @@ def search(b,L,waccm,data_list,test,label,init_weight):
         wvote += label[:,:,i]*weight[i]
         
       wvote = torch.Tensor(wvote.argmax(axis=1)).cuda()
-      acc = torch.sum(wvote==test)/4599
+      acc = torch.sum(wvote==test)/num
         # print(f"weight vote:{acc}") 
       
       if acc > waccm:
@@ -51,9 +54,9 @@ def search(b,L,waccm,data_list,test,label,init_weight):
         
     
 
-directory = './log'  
+directory = './train'  
 
-skip = [0,4,12]
+skip = [0,5]
 data_list = []
 name_list = []
 count = -1
@@ -76,38 +79,42 @@ for root, dirs, files in os.walk(directory):
 label = np.stack(data_list,axis=2)
 label = torch.from_numpy(label).cuda()
 
-test = np.load('/home/niyunfei/workspace/wuRenji/wuRenji/wuRenji/data/test_label.npy')
+test = np.load('/home/niyunfei/workspace/wuRenji/wuRenji/wuRenji/data/train_label.npy')
 test = torch.from_numpy(test).cuda()
 
 
 if __name__ == '__main__':
+    num = 16432
  
-    label_one_hot = torch.zeros((4599,155)).cuda()
-    for i in range(4599):
+    label_one_hot = torch.zeros((num,155)).cuda()
+    for i in range(num):
       label_one_hot[i,test[i]] = 1
   
     vote = 0
     wvote = 0
-    # weight = [64.2885, 72.2760, 61.1249, 70.2942, 84.6421, 79.3695, 69.4743, 58.7067,
-    #      73.5881, 57.4281]
+    # weight = [ 0.1419,  0.1419,  0.2274, -0.0611,  0.1439,  0.1262,  0.0693,  0.0842,
+    #       0.0767,  0.1104]
     
     for i in range(len(data_list)):
      
       vote += data_list[i]
-    #   wvote += data_list[i]*weight[len(data_list)-1-i]
+      # wvote += data_list[i]*weight[i]
       p = torch.Tensor(data_list[i].argmax(-1)).cuda()
-      acc = torch.sum(p==test)/4599
+      acc = torch.sum(p==test)/num
       print(f"model {name_list[i]} prev pred:{acc}")
     
     vote = torch.Tensor(vote.argmax(axis=1)).cuda()
-    acc = torch.sum(vote==test)/4599
-    print(f"vote pred:{acc}")    
+    acc = torch.sum(vote==test)/num
+    print(f"voted pred:{acc}")    
     
+    # np.save('pred.npy',wvote)
+    # print(wvote.shape)
     # wvote = torch.Tensor(wvote.argmax(axis=1)).cuda()
-    # acc = torch.sum(wvote==test)/4599
-    # print(f"weig vote:{acc}") 
-    
-    time.sleep(1)
+    # acc = torch.sum(wvote==test)/num
+    # print(f"weigb vote:{acc}") 
+ 
+
+    # time.sleep(1000)
     
     # # SEARCH
     # b=5
@@ -122,61 +129,84 @@ if __name__ == '__main__':
     class MLP(nn.Module):
       def __init__(self, c):
         super(MLP, self).__init__()
-        self.fc = nn.Linear(c,1,bias=False)
-        self.fc.weight.data = torch.Tensor([64.2885, 72.2760, 61.1249, 70.2942, 84.6421, 79.3695, 69.4743, 58.7067,
-         73.5881, 57.4281])
+        self.fc = nn.Sequential(
+          nn.Linear(c, 256, bias=False),
+          nn.ReLU(),
+          nn.Linear(256, 1,bias=False),
+        )
+        # self.fc = nn.Linear(c,1,bias=False)
+    
         
       def forward(self, x):
-       
+    
         x = self.fc(x).squeeze(-1)
         return x
       
-    total = 100000
+    total = 2000
     flip = 100
     loss_fn = nn.CrossEntropyLoss()
     loss_fn = loss_fn.cuda()
     mlp = MLP(label.shape[-1]).cuda()
-    optim = op.SGD(mlp.parameters(),lr=0.00001,momentum=0.9,weight_decay=0.0004)
+    optim = op.SGD(mlp.parameters(),lr=0.0001,momentum=0.9,weight_decay=0.0004)
     schedule = op.lr_scheduler.LambdaLR(optim, lr_lambda=[lambda flip:total*flip/(flip+i)])
     
-    weight = [64.2885, 72.2760, 61.1249, 70.2942, 84.6421, 79.3695, 69.4743, 58.7067,
-         73.5881, 57.4281]
+    weigh = [1 for _ in range(label.shape[-1])]
     accm = 0
     index = 0
-    eval = 0
+    eval = 1
+    mlp.load_state_dict(torch.load('vote.pt'))
     for _ in tqdm(range(total)):
         optim.zero_grad()
       
         out = mlp(label)
         loss = loss_fn(out,label_one_hot)
         
-        acc = torch.sum(out.argmax(dim=-1)==test)/4599
+        acc = torch.sum(out.argmax(dim=-1)==test)/num
+        
         if eval==1:
+            mlp.load_state_dict(torch.load('vote.pt'))
+            mlp.eval()
+            out = mlp(label)
+            acc = torch.sum(out.argmax(dim=-1)==test)/num
             print(f'acc:{acc}')
             break
         
         if acc > accm:
-          accm = acc
-          weight = mlp.fc.weight
-          index = _
+            print('best acc:{}'.format(acc))
+            accm = acc
+            torch.save(mlp.state_dict(),'vote.pt')
+            index = _
         
         loss.backward()
         optim.step()
         schedule.step()
-    print('best weight:{}\nbest acc:{} in {}'.format(weight,accm,index))    
-        
+    print('best acc:{} in {}'.format(accm,index))    
+    # pdb.set_trace()
+    
+    
+    
+    
+    
+    
+    
+    
     
     # Rubutness
-    # weight = [171.8091,  95.9382, 508.0397, 459.0803, 240.3197, 147.5455, 142.4117,
-    #      177.9169, 155.9857,  76.2838]
+    # weight = torch.load('74646.pt')
+    # import pdb
+    # pdb.set_trace()
+    # weight = [ 0.1419,  0.1419,  0.2274, -0.0611,  0.1439,  0.1262,  0.0693,  0.0842,
+    #       0.0767,  0.1104]
+    # # weight = torch.Tensor(weight).cuda().softmax(dim=0)
     # for j in range(9):
     #   wvm = 0
     #   std = np.std(weight)/10
+    #   mean = np.mean(weight)
     #   for i in range(len(data_list)):
     #     wvm += data_list[i]*(weight[i]+random.uniform(-std*(j+1),std*(j+1)))
         
     #   wvm = torch.Tensor(wvm.argmax(axis=1)).cuda()
-    #   accd = torch.sum(wvm==test)/4599
-    #   print('wvm:{},{} delta:{:4f}'.format(accd,0.01*(j+1),(accd-acc)/acc))
+    #   accd = torch.sum(wvm==test)/num
+    #   print('wvm:{},delta weight: {:f}% delta acc: {:f}%\n'.format(accd,100*std*(j+1)/mean,(accd-acc).abs()*100/acc))
     
    
